c57b5d3618d704d0c217b7076784e1e940548bf2
Abhinav Kumar <22006713+abhinav999k@users.noreply.github.com>
2018-06-11 23:30:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/abhin_000/bountyhunt/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.abhin_000.bountyhunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
