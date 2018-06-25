ecc15b095210eeed911b60a55af028dc8d371933
Abhinav Kumar <22006713+abhinav999k@users.noreply.github.com>
2018-06-25 21:47:26 +0530

    - UI Enhancments    + addition of signup page

--- app/src/main/java/com/example/abhin_000/bountyhunt/MainActivity.java
+++ app/src/main/java/com/example/abhin_000/bountyhunt/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.abhin_000.bountyhunt;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
     RelativeLayout rellay1, rellay2;
-
+    private Button signup_button;
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
         @Override
@@ -22,9 +24,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        signup_button = (Button)findViewById(R.id.signup_button);
         rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
         rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignUP_activity.class);
+                startActivity(intent);
+            }
+        });
 
         handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
     }
---
+++ app/src/main/java/com/example/abhin_000/bountyhunt/SignUP_activity.java
@@ -0,0 +1,13 @@
+package com.example.abhin_000.bountyhunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUP_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_activity);
+    }
+}
